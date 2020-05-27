09c53474d60467af9bbf004881b85250d92a0742
Erwin Winata <erwin@agate.id>
2020-05-28 00:01:20 +0700

    moved images from public/res to src/res

---
+++ src/res/index.tsx
@@ -0,0 +1,30 @@
+export const res_logo = require("./logo.png");
+
+export const res_discover = require("./discover.svg");
+export const res_discover_active = require("./discover_active.svg");
+export const res_library = require("./library.svg");
+export const res_library_active = require("./library_active.svg");
+export const res_listen = require("./listen.svg");
+export const res_listen_active = require("./listen_active.svg");
+
+export const res_like = require("./like.svg");
+export const res_like_active = require("./like_active.svg");
+
+export const res_play = require("./play.svg");
+export const res_pause = require("./pause.svg");
+export const res_prev = require("./prev.svg");
+export const res_next = require("./next.svg");
+export const res_repeat = require("./repeat.svg");
+export const res_repeat_all = require("./repeat_all.svg");
+export const res_repeat_one = require("./repeat_one.svg");
+export const res_shuffle = require("./shuffle.svg");
+export const res_shuffle_active = require("./shuffle_active.svg");
+
+export const res_save = require("./save.svg");
+export const res_merge = require("./merge.svg");
+
+export const res_option = require("./option.svg");
+export const res_plus = require("./plus.svg");
+export const res_video = require("./video.svg");
+
+export const res_close = require("./close.svg");
